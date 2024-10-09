cc89a36d109ba4adcbe1a3dc43833ce89ba452ec
Karl Floersch <karl@karlfloersch.com>
2024-10-08 20:17:52 -0600

    prepare ourselves for our first cross chain read

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { Predeploys } from "@contracts-bedrock/libraries/Predeploys.sol";
+import { ICrossL2Inbox } from "@contracts-bedrock/L2/interfaces/ICrossL2Inbox.sol";
+
 contract Counter {
     uint256 public number;
     event CounterDeployed(uint256 indexed magicNumber, address indexed contractAddress);
@@ -20,5 +23,10 @@ contract Counter {
     // Add getValue function
     function getValue() public view returns (uint256) {
         return number;
+    }
+
+    function testEmitRead(ICrossL2Inbox.Identifier calldata _newGameId, bytes calldata _newGameData) external returns (uint256) {
+        ICrossL2Inbox(Predeploys.CROSS_L2_INBOX).validateMessage(_newGameId, keccak256(_newGameData));
+        return 420;
     }
 }
