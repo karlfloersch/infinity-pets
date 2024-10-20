32ecdba747ebfb842336d74e855bdc102c7db59c
Karl Floersch <karl@karlfloersch.com>
2024-10-20 15:17:45 -0700

    WIP v2 structure

---
+++ src/v2/Counter2.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./SuperchainEnabled2.sol";
+import { ICrossL2Inbox } from "@contracts-bedrock/L2/interfaces/ICrossL2Inbox.sol";
+import { Predeploys } from "@contracts-bedrock/libraries/Predeploys.sol";
+
+contract Counter2 is SuperchainEnabled2 {
+    uint256 public number;
+
+    event CounterDeployed(uint256 indexed magicNumber, address indexed contractAddress);
+    event CounterIncremented(uint256 indexed newValue);
+
+    constructor() {
+        emit CounterDeployed(420, address(this));
+    }
+
+    /// @notice Sets the number to a new value
+    /// @param newNumber The new number to set
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    /// @notice Increments the number by 1
+    function increment() public {
+        number++;
+        emit CounterIncremented(number);
+    }
+
+    /// @notice Retrieves the current value of the counter
+    /// @return The current counter value
+    function getValue() public view returns (uint256) {
+        return number;
+    }
+
+    function testEmitRead(ICrossL2Inbox.Identifier calldata _eventId, bytes calldata _eventData) external returns (uint256) {
+        ICrossL2Inbox(Predeploys.CROSS_L2_INBOX).validateMessage(_eventId, keccak256(_eventData));
+        number += 420;
+        return 420;
+    }
+
+    /// @notice Sends a cross-chain message to increment the counter on another chain
+    /// @param destChainId The destination chain ID
+    function sendIncrementToChain(uint256 destChainId) external {
+        bytes memory data = abi.encodeWithSelector(this.crossChainIncrement.selector);
+        _xMessageSelf(destChainId, data);
+    }
+
+    /// @notice Handles incoming cross-chain messages to increment the counter
+    function crossChainIncrement() external xOnlyFromSelf {
+        number += 10;
+        emit CounterIncremented(number);
+    }
+}
---
+++ src/v2/RemoteCaller.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./SuperchainEnabled2.sol";
+import { ICrossL2Inbox } from "@contracts-bedrock/L2/interfaces/ICrossL2Inbox.sol";
+import { Predeploys } from "@contracts-bedrock/libraries/Predeploys.sol";
+
+contract RemoteCaller is SuperchainEnabled2 {
+    // TODO: Implement
+}
