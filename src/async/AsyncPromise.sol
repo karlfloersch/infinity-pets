450bb54c0fb13bd71b8d9a5298a1f9f38b564f33
ben-chain <ben@optimism.io>
2024-11-03 19:50:41 +0800

    improve promise state labels

--- src/async/AsyncPromise.sol
+++ src/async/AsyncPromise.sol
@@ -2,15 +2,16 @@ pragma solidity ^0.8.13;
 import {console} from "forge-std/console.sol";
 
 enum AsyncPromiseState {
-    AWAITING_CALLBACK_SELECTOR,
-    COMPLETED
+    WAITING_FOR_SET_CALLBACK_SELECTOR,
+    WAITING_FOR_CALLBACK_EXECUTION,
+    RESOLVED // as-yet unused
 }
 
 contract AsyncPromise {
     address public immutable localInvoker;
     bytes4 public callbackSelector;
     bytes32 public messageId;
-    AsyncPromiseState public state = AsyncPromiseState.AWAITING_CALLBACK_SELECTOR;
+    AsyncPromiseState public state = AsyncPromiseState.WAITING_FOR_SET_CALLBACK_SELECTOR;
 
     constructor(address _invoker, bytes32 _messageId) {
         localInvoker = _invoker;
@@ -20,18 +21,18 @@ contract AsyncPromise {
     fallback() external {
         require(msg.sender == localInvoker, "Only the caller can set this promise's callback");
 
-        if (state == AsyncPromiseState.COMPLETED) {
+        if (state == AsyncPromiseState.WAITING_FOR_CALLBACK_EXECUTION) {
             revert("Promise already setup");
         }
 
-        if (state == AsyncPromiseState.AWAITING_CALLBACK_SELECTOR) {
+        if (state == AsyncPromiseState.WAITING_FOR_SET_CALLBACK_SELECTOR) {
             // TODO: is there a way to confirm in the general case this is ".then"?
             console.log("got callback selector");
             console.logBytes(msg.data);
             // 4 bytes for the outer selector, 20 bytes for the address, 4 bytes for the callback selector
             // TODO: battle test this against more examples / confirm sufficiently generalized
             callbackSelector = bytes4(msg.data[24:28]);
-            state = AsyncPromiseState.COMPLETED;
+            state = AsyncPromiseState.WAITING_FOR_CALLBACK_EXECUTION;
         }
     }
 }
